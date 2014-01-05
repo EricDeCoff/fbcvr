8566f44a09c2f2447068e2e167fecd456e8d4ceb
Eric DeCoff <ericdecoff@gmail.com>
2014-01-05 01:00:47 -0500

    V1.0 ( Platform Removed - 15 mb limit )

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-c:\_phonegap3.0\FBCVR\platforms\android\bin\AndroidManifest.xml : \
-c:\_phonegap3.0\FBCVR\platforms\android\AndroidManifest.xml \
--- platforms/android/bin/FBCFR-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-c:\_phonegap3.0\FBCVR\platforms\android\bin\FBCFR-debug-unaligned.apk : \
-c:\_phonegap3.0\FBCVR\platforms\android\bin\FBCFR.ap_ \
-c:\_phonegap3.0\FBCVR\platforms\android\bin\classes.dex \
-c:\_phonegap3.0\FBCVR\platforms\android\libs\cordova-3.2.0.jar \
