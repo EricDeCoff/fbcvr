2386e496e949e794090b32c73555816b31551115
Eric DeCoff <ericdecoff@gmail.com>
2014-01-05 04:50:36 -0500

    V1 ( Added WebIntent )

--- www/js/getDirections.js
+++ www/js/getDirections.js
@@ -2,13 +2,11 @@
  * Created by Eric R DeCoff on 1/2/14.
  */
 function getDirections(address){
-    intel.xdk.device.launchExternal("http://maps.google.com/maps?q=1483 Hwy 78 W, Villa Rica, GA");
-    /*
+//    intel.xdk.device.launchExternal("http://maps.google.com/maps?q=1483 Hwy 78 W, Villa Rica, GA");
     window.plugins.webintent.startActivity({
             action: WebIntent.ACTION_VIEW,
             url: 'geo:0,0?q=' + address},
         function() {},
         function(e) {alert('Failed to open URL via Android Intent');}
     );
-    */
 };
